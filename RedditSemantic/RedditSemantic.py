edbc465611e473c5578b615ed4f4fb1fcc29f88e
higgyone <higgyone@hotmail.com>
2017-06-25 18:19:49 +0100

    Initial commit

---
+++ RedditSemantic/RedditSemantic.py
@@ -0,0 +1,63 @@
+import praw
+import time
+import datetime
+from RedditApiSecrets import RedditApiSecrets 
+from TextFileManipulation import TextFileManipulation
+
+outputFile = "data.txt"
+headers = []
+
+apiSecrets = RedditApiSecrets()
+tfm = TextFileManipulation(outputFile)
+
+tfm.RemoveFile()
+
+reddit = praw.Reddit(client_id= apiSecrets.GetClientId(),
+                     client_secret= apiSecrets.GetClientSecret(),
+                     user_agent= apiSecrets.GetUserAgent())
+
+print(reddit.read_only)
+
+subreddit = reddit.subreddit('unitedkingdom')
+
+print(subreddit.display_name)  
+print(subreddit.title)         
+print(subreddit.description)
+
+start_dt = datetime.datetime(year=2017, month=4, day=18, hour=11).timetuple()
+end_dt = datetime.datetime(year=2017, month=4, day=18, hour=20).timetuple()
+
+s_t = time.mktime(start_dt)
+e_t = time.mktime(end_dt)
+
+search_str = "timestamp:" + str(int(s_t)) + ".." + str(int(e_t))
+
+search_results = list(subreddit.search(search_str, syntax='cloudsearch', limit=None))
+
+i = 1
+
+if len(search_results) > 0:
+
+    for sub in search_results:
+        headers.append(str(i) + ": " + sub.title)
+        i +=1
+    tfm.WriteLine("Test")
+    tfm.WriteLines(headers)
+
+    #with open(outputFile, "a") as myfile:
+     #   for sub in search_results:
+      #      print(str(i) + ": " + sub.title)
+       #     date = datetime.datetime.fromtimestamp(sub.created_utc)
+        #    print(date)
+         #   myfile.write(str(i) + ": " + sub.title + "\n")
+          #  i = i + 1
+
+            
+    
+
+
+#for submission in subreddit.new(limit=100):
+#    print (submission.title)
+#    time = submission.created
+#    print(datetime.datetime.fromtimestamp(time))
+
---
+++ RedditSemantic/TextFileManipulation.py
@@ -0,0 +1,48 @@
+import os
+
+class TextFileManipulation(object):
+    """Read and write to text file helpers"""
+
+    def __init__(self, path = None):
+        self.path = path 
+
+    def RemoveFile(self, path = None):
+        filePath = path
+        if path == None:
+            filePath = self.path
+
+        if filePath == None:
+            print("No file path defined")
+            return
+
+        try:
+            os.remove(filePath)
+        except OSError as e:  ## if failed, report it back to the user ##
+            print ("Error: %s - %s." % (e.filename,e.strerror))
+
+
+    def WriteLine(self, text, path = None):
+        filePath = path
+        if path == None:
+            filePath = self.path
+
+        if filePath == None:
+            print("No file path defined")
+            return
+
+        with open(filePath, "a") as appendFile:
+            appendFile.write(text + "\n")
+
+    def WriteLines(self, text, path = None):
+        filePath = path
+        if path == None:
+            filePath = self.path
+
+        if filePath == None:
+            print("No file path defined")
+            return
+
+        with open(filePath, "a") as appendFile:
+            for line in text:
+                appendFile.write(line + "\n")
+            
